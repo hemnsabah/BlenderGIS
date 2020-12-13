dbd410f8677f04324b11fa394ba67c4fc428ec36
Hemn <hemnsalay@gmail.com>
2020-12-14 00:09:50 +0300

    added comment to geoscene.py

--- geoscene.py
+++ geoscene.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 #  ***** GPL LICENSE BLOCK *****
+# I added this comment for the coarse
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
